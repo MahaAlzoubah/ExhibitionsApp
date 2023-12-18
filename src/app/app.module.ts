ba6086cb5b008681ae046e6ada6d4e821510a77d
maha alzoubah <m202002565@hotmail.com>
2023-12-18 14:42:56 +0300

    linked to firebase

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,10 +6,30 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { provideFirestore, getFirestore } from '@angular/fire/firestore';
+
+
+
+const firebaseConfig = {
+  apiKey: "AIzaSyB36QGKb8l32Lk9yJcSg2R0ZJlXbVZrApc",
+  authDomain: "itcs444-finalproject.firebaseapp.com",
+  projectId: "itcs444-finalproject",
+  storageBucket: "itcs444-finalproject.appspot.com",
+  messagingSenderId: "167037167864",
+  appId: "1:167037167864:web:e0f5ab3717825ae0e5b30d",
+  measurementId: "G-55QRC7D0CC"
+};
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule,
+  // initialize angularfire with credentials from the dashboard
+  provideFirebaseApp(() => initializeApp(firebaseConfig)),
+  // Import the AngularFireDatabaseModule to use database
+  provideFirestore(() => getFirestore()),
+
+  ],
   providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
   bootstrap: [AppComponent],
 })
