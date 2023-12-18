a8791eacb47c470ac6ca1820cd3c1c0c41ef811d
maha alzoubah <m202002565@hotmail.com>
2023-12-18 17:09:57 +0300

    update service

--- src/app/fbservice.service.ts
+++ src/app/fbservice.service.ts
@@ -1,9 +1,94 @@
 import { Injectable } from '@angular/core';
+import { Firestore } from '@angular/fire/firestore';
+import { collection, collectionData, CollectionReference, DocumentReference } from '@angular/fire/firestore';
+import { getDocs, doc, deleteDoc, updateDoc, docData, setDoc, addDoc, query } from '@angular/fire/firestore';
+import { Data } from '@angular/router';
+import { DocumentData } from 'firebase/firestore';
+import { Observable } from 'rxjs';
+import {
+    Auth,
+    getAuth,
+    signInWithEmailAndPassword,
+    createUserWithEmailAndPassword,
+    signOut,
+    sendSignInLinkToEmail
+  } from '@angular/fire/auth';
+import { Router } from '@angular/router';
+  
+export interface Events {
+  id: string;
+  Agenda: string;
+  Attendees: string[];
+  Exhibition: string;
+  Speakers: string[];
+  Updates: string;
+  UserID: string;
+}
+
+export interface Halls {
+  id: string;
+  Availability: string;
+  BusinessContact: string;
+  Capacity: string;
+  HallName: string;
+  HallNum: string;
+  Image: string;
+  NumBooths: string;
+}
+
+export interface ReservationEvents {
+  EventID: string;
+  UserID: string;
+}
+
+export interface ReservationHalls {
+  HallNum: string;
+  UserID: string;
+}
+
+export interface ReservationRequest {
+  Date: Data;
+  HallNum: string;
+  Status: 'Pending';
+  UserID: string;
+}
+
+export interface Users {
+  Email: string;
+  Name: string;
+  Password: string;
+  Type: string;
+}
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class FBserviceService {
 
-  constructor() { }
+  public events$: Observable<Events[]> | undefined;
+  public eventsCollection: CollectionReference<DocumentData>;
+
+  public halls$: Observable<Halls[]> | undefined;
+  public hallsCollection: CollectionReference<DocumentData>;
+
+  public reservationEvents$: Observable<ReservationEvents[]> | undefined;
+  public rEventsCollection: CollectionReference<DocumentData>;
+
+  public reservationHalls$: Observable<ReservationEvents[]> | undefined;
+  public rHallsCollection: CollectionReference<DocumentData>;
+
+  public reservationRequest$: Observable<ReservationRequest[]> | undefined;
+  public rRequestCollection: CollectionReference<DocumentData>;
+
+  public users$: Observable<Users[]> | undefined;
+  public usersCollection: CollectionReference<DocumentData>;
+
+  constructor(public firestore: Firestore, public auth:Auth, public router: Router) {
+    this.eventsCollection = collection(this.firestore, 'events');
+    this.hallsCollection = collection(this.firestore, 'halls');
+    this.rEventsCollection = collection(this.firestore, 'reservationEvents');
+    this.rHallsCollection = collection(this.firestore, 'reservationHalls'); 
+    this.rRequestCollection = collection(this.firestore, 'reservationRequest');
+    this.usersCollection = collection(this.firestore, 'users');
+  }
 }
--- src/app/profile/profile.page.ts
+++ src/app/profile/profile.page.ts
@@ -1,26 +1,42 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { FBserviceService } from '../fbservice.service';
+import { getAuth, onAuthStateChanged} from "firebase/auth";
+import { Auth } from '@angular/fire/auth';
+import { getDocs, doc, deleteDoc, updateDoc, docData, setDoc, addDoc, query, Firestore, getDoc, arrayUnion, where,collection } from '@angular/fire/firestore';
+
 
 @Component({
   selector: 'app-profile',
   templateUrl: './profile.page.html',
   styleUrls: ['./profile.page.scss'],
 })
-export class ProfilePage implements OnInit {
-  userName: string = 'John Doe'; // Replace with actual user name
-  userEmail: string = 'john@example.com'; // Replace with actual user email
+export class ProfilePage {
+  userName: string = 'John Doe';
+  userEmail: string = 'john@example.com';
   reservedHalls: any[] = [
     { name: 'Hall A' },
     { name: 'Hall B' },
-    // Add more reserved halls as needed
+    { name: 'Hall C' },
+    { name: 'Hall C' },
+    { name: 'Hall C' },
+    { name: 'Hall C' },
+    { name: 'Hall C' },
+    { name: 'Hall C' },
+    { name: 'Hall C' },
   ];
   reservedEvents: any[] = [
     { name: 'Event X' },
     { name: 'Event Y' },
-    // Add more reserved events as needed
   ];
-  constructor() { }
+  accordionState: { [key: string]: boolean } = {};
 
-  ngOnInit() {
+  constructor() {}
+
+  toggleAccordion(section: string): void {
+    this.accordionState[section] = !this.accordionState[section];
   }
 
+  isAccordionOpen(section: string): boolean {
+    return this.accordionState[section] || false;
+  }
 }
